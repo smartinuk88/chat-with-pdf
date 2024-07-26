21cc7028a1311787742e095a0145632a3b91e813
Scott Martin <scott.martin88@live.com>
2024-07-26 07:54:32 +0200

    Build PDFView

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,4 +1,30 @@
-function ChatToFilePage({ params: { id } }: { params: { id: string } }) {
-  return <div>Chat To File Page: {id}</div>;
+import Chat from "@/components/Chat";
+import PDFView from "@/components/PDFView";
+import { adminDb } from "@/firebaseAdmin";
+import { auth } from "@clerk/nextjs/server";
+
+async function ChatToFilePage({ params: { id } }: { params: { id: string } }) {
+  auth().protect();
+  const { userId } = await auth();
+
+  const ref = await adminDb
+    .collection("users")
+    .doc(userId!)
+    .collection("files")
+    .doc(id)
+    .get();
+
+  const url = ref.data()?.downloadUrl;
+
+  return (
+    <div className="grid lg:grid-cols-5 h-full overflow-hidden">
+      <div className="lg:col-span-2 overflow-y-auto">
+        <Chat id={id} />
+      </div>
+      <div className="lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:order-1 overflow-auto">
+        <PDFView url={url} />
+      </div>
+    </div>
+  );
 }
 export default ChatToFilePage;
---
+++ components/Chat.tsx
@@ -0,0 +1,4 @@
+function Chat({ id }: { id: string }) {
+  return <div>Chat</div>;
+}
+export default Chat;
