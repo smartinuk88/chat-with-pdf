ece0fbf95f3342fdfdcf0d92c21a82910b789fe4
Scott Martin <scott.martin88@live.com>
2024-07-26 13:02:02 +0200

    Build chat functionality

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -18,11 +18,12 @@ async function ChatToFilePage({ params: { id } }: { params: { id: string } }) {
 
   return (
     <div className="grid lg:grid-cols-5 h-full overflow-hidden">
-      <div className="lg:col-span-2 overflow-y-auto">
-        <Chat id={id} />
-      </div>
       <div className="lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:order-1 overflow-auto">
         <PDFView url={url} />
+      </div>
+
+      <div className="lg:col-span-2 overflow-y-auto">
+        <Chat id={id} />
       </div>
     </div>
   );
--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,4 +1,156 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { FormEvent, useEffect, useRef, useState, useTransition } from "react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Loader2Icon } from "lucide-react";
+import { collection, orderBy, query } from "firebase/firestore";
+import { db } from "@/firebase";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { askQuestion } from "@/actions/askQuestion";
+import ChatMessage from "./ChatMessage";
+
+export type Message = {
+  id?: string;
+  role: "human" | "ai" | "placeholder";
+  message: string;
+  createdAt: Date;
+};
+
 function Chat({ id }: { id: string }) {
-  return <div>Chat</div>;
+  const { user } = useUser();
+  const [input, setInput] = useState("");
+  const [isPending, startTransition] = useTransition();
+  const [messages, setMessages] = useState<Message[]>([]);
+  const bottomOfChatRef = useRef<HTMLDivElement>(null);
+
+  const [snapshot, loading, error] = useCollection(
+    user &&
+      query(
+        collection(db, "users", user?.id, "files", id, "chat"),
+        orderBy("createdAt", "asc")
+      )
+  );
+
+  useEffect(() => {
+    bottomOfChatRef.current?.scrollIntoView({
+      behavior: "smooth",
+    });
+  }, [messages]);
+
+  useEffect(() => {
+    if (!snapshot) return;
+
+    console.log("Updated snapshot", snapshot.docs);
+
+    // Get second last message to check AI is thinking
+    const lastMessage = messages.pop();
+
+    if (lastMessage?.role === "ai" && lastMessage.message === "Thinking...") {
+      // Return as this is a dummy placeholder message
+      return;
+    }
+
+    const newMessages = snapshot.docs.map((doc) => {
+      const { role, message, createdAt } = doc.data();
+
+      return {
+        id: doc.id,
+        role,
+        message,
+        createdAt: createdAt.toDate(),
+      };
+    });
+
+    setMessages(newMessages);
+  }, [snapshot]);
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const q = input;
+
+    setInput("");
+
+    // Optimistic UI Update
+    setMessages((prev) => [
+      ...prev,
+      {
+        role: "human",
+        message: q,
+        createdAt: new Date(),
+      },
+      {
+        role: "ai",
+        message: "Thinking...",
+        createdAt: new Date(),
+      },
+    ]);
+
+    startTransition(async () => {
+      const { success, message } = await askQuestion(id, q);
+
+      if (!success) {
+        setMessages((prev) =>
+          prev.slice(0, prev.length - 1).concat([
+            {
+              role: "ai",
+              message: `Whoops... ${message}`,
+              createdAt: new Date(),
+            },
+          ])
+        );
+      }
+    });
+  };
+
+  return (
+    <div className="flex flex-col h-full overflow-scroll">
+      <div className="flex-1 w-full">
+        {loading ? (
+          <div className="flex items-center justify-center">
+            <Loader2Icon className="animate-spin h-20 w-20 text-indigo-600 mt-20" />
+          </div>
+        ) : (
+          <div className="p-5">
+            {messages.length === 0 && (
+              <ChatMessage
+                key={"placeholder"}
+                message={{
+                  role: "ai",
+                  message: "Ask me anything about the document!",
+                  createdAt: new Date(),
+                }}
+              />
+            )}
+
+            {messages.map((message, index) => (
+              <ChatMessage key={index} message={message} />
+            ))}
+            <div ref={bottomOfChatRef}></div>
+          </div>
+        )}
+      </div>
+
+      <form
+        onSubmit={handleSubmit}
+        className="flex sticky bottom-0 space-x-2 p-5 bg-indigo-600/75"
+      >
+        <Input
+          placeholder="Ask a Question..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <Button type="submit" disabled={!input || isPending}>
+          {isPending ? (
+            <Loader2Icon className="animate-spin text-indigo-600" />
+          ) : (
+            "Ask"
+          )}
+        </Button>
+      </form>
+    </div>
+  );
 }
 export default Chat;
